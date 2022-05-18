211e6d07707275e5dad8713934f32f5ca7dd7ea2
Lorrans Facca <59665341+lorransfacca@users.noreply.github.com>
2022-05-18 18:20:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+        const btn = document.getElementById("contraste");
+
+            btn.addEventListener("click", function() {
+                if (document.body.classList.contains("light")) {
+                    document.body.classList.remove("light");
+                    document.body.classList.add("dark");
+                } else {
+                    document.body.classList.remove("dark");
+                    document.body.classList.add("light");
+                }
+            });
